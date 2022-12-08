0622dc401f9cd41b16b139de17b053e09c9c1469
Tereshchenko Vlad <ddiavall@yandex.ru>
2022-12-08 18:27:41 +0300

    [Feat]: added profile objects and logic

--- ITMO-Donuts-iOS/Models/UserData.swift
+++ ITMO-Donuts-iOS/Models/UserData.swift
@@ -9,4 +9,9 @@ import Foundation
 
 struct UserData {
     static var username = ""
+    static var first_name = ""
+    static var second_name = ""
+    static var phone = ""
+    static var email = ""
+    static var balance = 10000
 }
